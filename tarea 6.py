a097758b90a425c54fe0694283ca1769eac9f846
paularays <144204494+paularays@users.noreply.github.com>
2023-11-16 20:22:13 -0600

    first commit

---
+++ tarea 6.py
@@ -0,0 +1,12 @@
+#Tarea 6
+miLista=[]
+for i in range (10):
+   numeros=float(input("agrega 10 numeros"))
+   numeros.append(numeros)
+
+   mayor=numeros[0]
+   menor=[0]
+
+   suma=0
+
+for numeros in miLista:
